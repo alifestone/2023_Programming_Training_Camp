d30c3ca6d5d758f25c43eb8acea6b8dccc755c7e
Dumbledore MHR <95558265+dumbledore96@users.noreply.github.com>
2024-07-02 11:23:41 +0800

    更新 main.blocks, main.ts, main.py

--- main.py
+++ main.py
@@ -1,3 +1,19 @@
+old: game.LedSprite = None
+block: game.LedSprite = None
+_this = 0
+basic.clear_screen()
+game.set_score(0)
+
 def on_forever():
-    pass
+    global _this, block, old
+    _this = 0
+    block = game.create_sprite(randint(0, 4), 0)
+    old = game.create_sprite(6, 6)
+    while block.get(LedSpriteProperty.Y) < 4:
+        basic.pause(500)
+        block.change(LedSpriteProperty.Y, 1)
+        if block.is_touching(old):
+            game.game_over()
+    old = game.create_sprite(block.get(LedSpriteProperty.X),
+        block.get(LedSpriteProperty.Y))
 basic.forever(on_forever)
--- main.ts
+++ main.ts
@@ -1,4 +1,18 @@
-basic.showNumber(1)
+let old: game.LedSprite = null
+let block: game.LedSprite = null
+let _this = 0
+basic.clearScreen()
+game.setScore(0)
 basic.forever(function () {
-	
+    _this = 0
+    block = game.createSprite(randint(0, 4), 0)
+    old = game.createSprite(6, 6)
+    while (block.get(LedSpriteProperty.Y) < 4) {
+        basic.pause(500)
+        block.change(LedSpriteProperty.Y, 1)
+        if (block.isTouching(old)) {
+            game.gameOver()
+        }
+    }
+    old = game.createSprite(block.get(LedSpriteProperty.X), block.get(LedSpriteProperty.Y))
 })
