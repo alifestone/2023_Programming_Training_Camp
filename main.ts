89f33cd578f7731c535a57c7a9997edea16b3ceb
alifestone <95862568+alifestone@users.noreply.github.com>
2024-07-08 13:30:30 +0800

    更新 main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,34 +1,40 @@
+input.onButtonPressed(Button.A, function () {
+    block.change(LedSpriteProperty.Y, -1)
+    basic.pause(50)
+    block.change(LedSpriteProperty.Y, -1)
+    basic.pause(50)
+    block.change(LedSpriteProperty.Y, -1)
+    basic.pause(400)
+    block.change(LedSpriteProperty.Y, 1)
+    basic.pause(50)
+    block.change(LedSpriteProperty.Y, 1)
+    basic.pause(50)
+    block.change(LedSpriteProperty.Y, 1)
+})
+input.onButtonPressed(Button.B, function () {
+    while (true) {
+        仙人掌 = game.createSprite(4, 4)
+        仙人掌2 = game.createSprite(4, 3)
+        for (let index = 0; index < 5; index++) {
+            basic.pause(500)
+            仙人掌.change(LedSpriteProperty.X, -1)
+            仙人掌2.change(LedSpriteProperty.X, -1)
+            if (仙人掌.isTouching(block) || 仙人掌2.isTouching(block)) {
+                game.gameOver()
+            }
+        }
+        仙人掌.delete()
+        仙人掌2.delete()
+        basic.pause(randint(500, 1500))
+    }
+})
 let 仙人掌2: game.LedSprite = null
 let 仙人掌: game.LedSprite = null
+let block: game.LedSprite = null
 basic.clearScreen()
-let block = game.createSprite(1, 4)
-basic.forever(function () {
-    仙人掌 = game.createSprite(4, 4)
-    仙人掌2 = game.createSprite(4, 3)
-    for (let index = 0; index < 5; index++) {
-        basic.pause(500)
-        仙人掌.change(LedSpriteProperty.X, -1)
-        仙人掌2.change(LedSpriteProperty.X, -1)
-        if (仙人掌.isTouching(block) || 仙人掌2.isTouching(block)) {
-            game.gameOver()
-        }
-    }
-    仙人掌.delete()
-    仙人掌2.delete()
-    basic.pause(randint(500, 1500))
-})
+block = game.createSprite(1, 4)
 basic.forever(function () {
     if (input.buttonIsPressed(Button.B)) {
-        block.change(LedSpriteProperty.Y, -1)
-        basic.pause(50)
-        block.change(LedSpriteProperty.Y, -1)
-        basic.pause(50)
-        block.change(LedSpriteProperty.Y, -1)
-        basic.pause(400)
-        block.change(LedSpriteProperty.Y, 1)
-        basic.pause(50)
-        block.change(LedSpriteProperty.Y, 1)
-        basic.pause(50)
-        block.change(LedSpriteProperty.Y, 1)
+    	
     }
 })
